4b7389588260f34a6dd66efe3d489b81d2e1357d
tyt <123113>
2021-07-28 08:42:16 +0800

    修改了注册模型类信息

--- bookmananger/admin.py
+++ bookmananger/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from bookmananger.models import BookInfo, PersonInfo
-# 注册模型类
+# 注册模型类BookInfo
+# 注册模型类PersonInfo
 admin.site.register(BookInfo)
 admin.site.register(PersonInfo)
